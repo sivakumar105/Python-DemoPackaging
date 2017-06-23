fca6cc23fd13bd6129d51996fb76137aeb370137
sivakumar105 <sivav.sf1@gmail.com>
2017-06-23 18:04:19 +0530

    fixed issue

--- python_demopackaging/python_demopackaging.py
+++ python_demopackaging/python_demopackaging.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 
 def hello():
     """ Returns a Hello World"""
-    return("Hello, World! Siva")
+    return("Hello, World!")
 
 def say_hello():
     """ Prints Hello World"""
